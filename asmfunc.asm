e360947abd273c7dcdcc8d5b65b8f14901db4688
kenwin728 <kenwin728@gmail.com>
2024-03-25 13:54:26 +0800

    Implemented dot product in C

--- asmfunc.asm
+++ asmfunc.asm
@@ -1,20 +1,20 @@
 ;Dot product function for x86-64
 section .data
-var1 dq 0.0
+var1 dw 0.0
 section .text
 bits 64
 default rel
 
-global dotproduct
+global dotproductinasm
 
-dotproduct:
+dotproductinasm:
 	xor RBX, RBX ;counter
-	movsd xmm0, [var1]
+	movss xmm0, [var1]
 	L1: movsd xmm1, [RDX + RBX]
-	movsd xmm2, [R8 + RBX]
-	mulsd xmm1, xmm2
-	addsd xmm0, xmm1
-	add RBX, 8
+	movss xmm2, [R8 + RBX]
+	mulss xmm1, xmm2
+	addss xmm0, xmm1
+	add RBX, 4
 	LOOP L1
 
 	ret
--- main.c
+++ main.c
@@ -2,14 +2,23 @@
 #include <stdlib.h>
 #include <time.h>
 
-extern double dotproduct(int n, double* vec1, double* vec2);
+extern float dotproductinasm(int n, float* vec1, float* vec2);
+
+float dotproductinc(int n, float vec1[], float vec2[]) {
+	float sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += (vec1[i] * vec2[i]);
+	}
+	return sum;
+}
 int main() {
 	int n = 3;
-	double vec1[3] = {1.2, 2.0, 3.5};
-	double vec2[3] = {2.0, 3.0, 7.2};
-	double sdot = dotproduct(n, vec1, vec2);
-	printf("Answer is %lf", sdot);
-
+	float vec1[3] = {1.2, 2.0, 3.5};
+	float vec2[3] = {2.0, 3.0, 7.2};
+	float sdot = dotproductinasm(n, vec1, vec2);
+	printf("Answer of assembly is %f\n", sdot);
+	sdot = dotproductinc(n, vec1, vec2);
+	printf("Answer of c is %f", sdot);
 
 
 	return 0;
